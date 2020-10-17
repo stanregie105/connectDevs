b672eb27650970180d4cfb73078370f10a8ce331
stanregie105 <stanleyregal004@yahoo.com>
2020-10-18 00:42:45 +0800

    modified config/db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 //const db = config.get('mongoURI')
 require('dotenv').config();
-const db = process.env.mongoURI;
+const db = process.env.mongoURi;
 const connectDB = async () => {
 	try {
 		await mongoose.connect(db, {
